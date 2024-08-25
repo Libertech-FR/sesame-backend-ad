cabbb7537fcc887c1db036cdb386ef4554e8dc6d
Alain Abbas <alain.abbas@libertech.fr>
2024-08-25 17:43:16 +0200

    missed lib

---
+++ src/lib/ad_utils.py
@@ -0,0 +1,104 @@
+import os.path
+import paramiko
+from jinja2 import FileSystemLoader
+import src.lib.backend_utils as u
+import jinja2
+
+__DEBUG__=0
+def set_config(config):
+    u.__CONFIG__ = config
+
+def open_ssh_conn():
+    pkey = paramiko.Ed25519Key.from_private_key_file('../.ssh/id_ed25519')
+    client = paramiko.SSHClient()
+    policy = paramiko.AutoAddPolicy()
+    client.set_missing_host_key_policy(policy)
+    host = u.config('host')
+    user = u.config('user')
+    try:
+        client.connect(host, username=user, pkey=pkey)
+        return client
+    except paramiko.ssh_exception.SSHException as e:
+        e_dict = e.args[0]
+        print(u.returncode(1, "Erreur d'authentification"))
+        exit(1)
+
+
+def exec_cmd(command):
+    client=open_ssh_conn()
+    stdin, stdout, stderr = client.exec_command(command)
+    content=stdout.read().decode()
+    del client, stdin, stdout, stderr
+    return content
+
+def compose_dn(entity):
+    rdnValue=u.find_key(entity,'cn')
+    branchAttr=u.config('branchAttr','')
+    branch  = ''
+    if branchAttr != '':
+        branchValue=u.find_key(entity,branchAttr)
+
+        match branchValue:
+            case 'etd':
+                branch=u.config('branchForEtd','')
+            case 'esn':
+                branch = u.config('branchForEsn', '')
+            case 'adm':
+                branch = u.config('branchForAdm', '')
+    if branch != '':
+        return 'cn=' + rdnValue+ ',' + branch + "," + u.config('base')
+    else:
+        return 'cn=' + rdnValue+  "," + u.config('base')
+
+def dn_superior(dn):
+   tab=dn.split(',')
+   tab.pop(0)
+   return ','.join(tab)
+
+
+def gen_script_from_template(entity,template):
+    data={
+        'domain' :u.config('domain'),
+        'base': u.config('base'),
+        'dn' : compose_dn(entity),
+        'path': dn_superior(compose_dn(entity)),
+        'e': u.make_entry_array(entity)
+    }
+    environment = jinja2.Environment(loader=FileSystemLoader("../ps1_templates/"))
+    template = environment.get_template(template)
+    content=template.render(data)
+    return content
+
+def ad_exec_script(entity,template,params=""):
+    content=gen_script_from_template(entity,template)
+    client = open_ssh_conn()
+    sshfile = client.open_sftp()
+    pid=os.getpid()
+    if __DEBUG__ == 0 :
+        scriptName='sesame_script.' + str(pid) + '.ps1'
+    else:
+        scriptName='sesame_script.ps1'
+    with sshfile.open(scriptName, mode="w") as message:
+        message.write(content)
+    ##execution du script
+    chan = client.get_transport().open_session()
+    if params == '':
+        cmd=scriptName
+    else:
+        cmd=scriptName + " " + params
+    chan.exec_command('powershell -ExecutionPolicy Bypass -NonInteractive -File ' + cmd)
+    exitCode = chan.recv_exit_status()
+    content = chan.recv(4096).decode('utf-8')
+    error = chan.recv_stderr(4096).decode()
+    if __DEBUG__ == 0:
+        chan = client.get_transport().open_session()
+        ##supression du script
+        chan.exec_command('del ' + scriptName)
+    del client
+    if exitCode == 0:
+        print(u.returncode(0,content.rstrip("\n")))
+        exit(0)
+    else:
+        print(u.returncode(1,content.rstrip("\n")))
+        exit(1)
+
---
+++ src/lib/backend_utils.py
@@ -0,0 +1,91 @@
+import configparser
+import json
+from sys import stdin
+
+__CONFIG__=configparser.RawConfigParser()
+
+
+def read_config(file):
+    with open(file) as f:
+        file_content = '[config]\n' + f.read()
+    __CONFIG__.read_string(file_content)
+    return __CONFIG__
+
+def config(key,default=''):
+    c=__CONFIG__['config']
+    return c.get(key,default)
+
+
+def readjsoninput():
+    input = stdin.read()
+    return json.loads(input)
+
+
+def returncode(code,message):
+    '''
+        Retourne le code au format json pour le backend
+    '''
+    data={}
+    data['code']=code
+    data['message']=message
+    return json.dumps(data)
+
+def is_backend_concerned(entity):
+    peopleType=find_key(entity,config('branchAttr'))
+    listBackend=config('backendFor')
+    if (listBackend.find(peopleType) == -1):
+        return False
+    return True
+
+def find_key(element, key):
+    '''
+    Check if *keys (nested) exists in `element` (dict).
+    '''
+    return _finditem(element,key)
+
+def _finditem(obj, key):
+    if key in obj: return obj[key]
+    for k, v in obj.items():
+        if isinstance(v,dict):
+            item = _finditem(v, key)
+            if item is not None:
+                return item
+
+def make_entry_array(entity):
+    data={}
+    if "identity" in entity['payload']:
+        objectclasses = entity['payload']['identity']['identity']['additionalFields']['objectClasses']
+        inetOrgPerson=entity['payload']['identity']['identity']['inetOrgPerson']
+        additionalFields=entity['payload']['identity']['identity']['additionalFields']['attributes']
+
+    else:
+        objectclasses=entity['payload']['additionalFields']['objectClasses']
+        inetOrgPerson = entity['payload']['inetOrgPerson']
+        additionalFields = entity['payload']['additionalFields']['attributes']
+    #inetOrgPerson
+    for k,v in inetOrgPerson.items():
+        data[k]=str(v)
+
+    for obj in objectclasses:
+        for k,v in additionalFields[obj].items():
+            data[k]=str(v)
+    return data
+
+
+def make_objectclass(entity):
+    data = {}
+    if "identity" in entity['payload']:
+        objectclasses = entity['payload']['identity']['identity']['additionalFields']['objectClasses']
+    else:
+        objectclasses = entity['payload']['additionalFields']['objectClasses']
+
+    return ['top', 'inetOrgPerson'] + objectclasses
+
+
+def make_entry_array_without_empty(entity):
+    data={}
+    data1=make_entry_array(entity)
+    for k,v in data1.items():
+        if str(v) != "":
+            data[k]=v
+    return data
